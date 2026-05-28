80ba8c3b1bb400b9dad985c05a506e5cb7c644b7
clairey333 <claire.marina161@gmail.com>
2026-05-28 21:11:28 +0000

    tried to make legend...failed

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ print(pancakes.nlargest(5, 'winpercent'))
 
 x = np.asarray(cleats["competitorname"])
 y = np.asarray(cleats["winpercent"])
-plt.figure(figsize = (10, 7))
+
+plt.figure(figsize = (15, 10))
 plt.bar(x,y, color = ['#4AE8CB', '#FAC8A5', '#F7E9A3', '#FAA17A', '#A9F5D9'])
 plt.title('top 5 most popular candies!')
 plt.xlabel('names of candy')
 plt.ylabel('percentage')
 plt.tight_layout()
-plt.legend(#maybe put stuff in here for the key)
+
+plt.legend(["Twix"])
+
 plt.savefig("candyy.png")
